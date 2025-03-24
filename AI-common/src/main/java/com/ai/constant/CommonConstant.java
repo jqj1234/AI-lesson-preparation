2d6814e5fea047e9e79a9d59206bb9003fb6b52c
蒋青江 <2959878253@qq.com>
2025-03-24 22:23:45 +0800

    初始工程

---
+++ AI-common/src/main/java/com/ai/constant/CommonConstant.java
@@ -0,0 +1,10 @@
+package com.ai.constant;
+
+/**
+ * @author ：褚婧雯
+ * @date ：2025/3/24 21:30
+ * @description ：常量信息类
+ */
+public class CommonConstant {
+    public static final String CHECK_PREFIX="CK:";
+}
---
+++ AI-server/src/main/java/com/ai/Application.java
@@ -0,0 +1,16 @@
+package com.ai;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ：褚婧雯
+ * @date ：2025/3/24 21:42
+ * @description ：引导类程序
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+}
